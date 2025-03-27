708737c1f9083921ccdcd79789d9ce07309394de
Dinesh9657803055 <dinesh.kadam1509@gmail.com>
2025-03-27 15:06:08 +0530

    New changes

--- src/main/java/com/qa/myntra/pages/HomePage.java
+++ src/main/java/com/qa/myntra/pages/HomePage.java
@@ -15,7 +15,7 @@ public class HomePage {
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 		
-		System.out.println("Dhruval Patel");
+		System.out.println("Dinesh bhau"); //new changes
 	}
 	
 	By menCategoryLink = By.xpath("//header[@id='desktop-header-cnt']//nav//div[contains(@class,'navContent')]//a[@data-group='men']");
