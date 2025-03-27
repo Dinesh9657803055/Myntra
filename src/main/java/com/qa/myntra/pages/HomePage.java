2678718805c3927d33e8c63c1ace34f3efa9f840
dhruvalpatel91 <smart.dhru11@gmail.com>
2025-03-27 13:00:17 +0530

    Test

--- src/main/java/com/qa/myntra/pages/HomePage.java
+++ src/main/java/com/qa/myntra/pages/HomePage.java
@@ -9,10 +9,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
+	
 	private WebDriver driver;
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
+		
+		System.out.println("Dhruval");
 	}
 	
 	By menCategoryLink = By.xpath("//header[@id='desktop-header-cnt']//nav//div[contains(@class,'navContent')]//a[@data-group='men']");
